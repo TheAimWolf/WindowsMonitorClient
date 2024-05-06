f21536dc878f0fb2ecc14880a5e7f886d43bf216
TheAimWolf <60583716+TheAimWolf@users.noreply.github.com>
2024-05-06 21:26:40 +0200

    Update page layout and add connection button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,23 +6,33 @@
             <table class="w-full text-xl">
                 <tr>
                     <td class="py-2">Server</td>
-                    <td class="py-2">
+                    <td class="py-2 text-right">
                         <input class="p-1 border-solid border-2 rounded-lg border-black" type="text" name="server" id="server" placeholder="support.juwolf.com">
                     </td>
                 </tr>
                 <tr>
                     <td class="py-2">Username</td>
-                    <td class="py-2">
+                    <td class="py-2 text-right">
                         <input class="p-1 border-solid border-2 rounded-lg border-black" type="text" name="username" id="username">
                     </td>
                 </tr>
                 <tr>
                     <td class="py-2">Passwort</td>
-                    <td class="py-2">
+                    <td class="py-2 text-right">
                         <input class="p-1 border-solid border-2 rounded-lg border-black" type="password" name="password" id="password">
                     </td>
                 </tr>
             </table>
+            <div class="w-full flex justify-between">
+                <h2 class="text-lg text-red-500">Getrennt</h2>
+                <button class="border-solid bg-slate-700 hover:bg-slate-500 rounded-lg text-white p-2">Verbinden</button>
+            </div>
+            <div class="w-full">
+                <h2>Log</h2>
+                <div class="border-solid border-black border-2 h-60 rounded-lg">
+                    <p class="p-1">Hier steht ein Log text</p>
+                </div>
+            </div>
         </div>
         <div class="w-1/2 text-right">
             <p>2</p>
