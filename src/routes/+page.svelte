202e1fa5d8e9c132c92e7d27889e65ca35a65a57
TheAimWolf <60583716+TheAimWolf@users.noreply.github.com>
2024-05-06 21:10:56 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<div class="w-full flex flex-col place-items-center">
+    <h1 class="text-3xl">Windows Monitor BETA</h1>
+    <h1 class="text-xl">Made witch ♥️ by AimWolf</h1>
+    <div class="flex w-full px-4 pt-10">
+        <div class="w-1/2">
+            <table class="w-full text-xl">
+                <tr>
+                    <td class="py-2">Server</td>
+                    <td class="py-2">
+                        <input class="p-1 border-solid border-2 rounded-lg border-black" type="text" name="server" id="server" placeholder="support.juwolf.com">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="py-2">Username</td>
+                    <td class="py-2">
+                        <input class="p-1 border-solid border-2 rounded-lg border-black" type="text" name="username" id="username">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="py-2">Passwort</td>
+                    <td class="py-2">
+                        <input class="p-1 border-solid border-2 rounded-lg border-black" type="password" name="password" id="password">
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="w-1/2 text-right">
+            <p>2</p>
+        </div>
+    </div>
+</div>
