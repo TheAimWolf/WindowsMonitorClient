ba6990b547d0f9cbd7f4584fdedca61a9e80203e
TheAimWolf <60583716+TheAimWolf@users.noreply.github.com>
2024-05-07 11:10:37 +0200

    Finish UI Layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,29 @@
-<div class="w-full flex flex-col place-items-center">
+<div class="w-full h-screen flex flex-col place-items-center overflow-hidden bg-zinc-800 text-white">
     <h1 class="text-3xl">Windows Monitor BETA</h1>
     <h1 class="text-xl">Made witch ♥️ by AimWolf</h1>
     <div class="flex w-full px-4 pt-10">
-        <div class="w-1/2">
+        <div class="w-full pr-1">
             <table class="w-full text-xl">
                 <tr>
                     <td class="py-2">Server</td>
                     <td class="py-2 text-right">
-                        <input class="p-1 border-solid border-2 rounded-lg border-black" type="text" name="server" id="server" placeholder="support.juwolf.com">
+                        <input class="p-1 border-solid border-2 rounded-lg border-white text-right bg-zinc-700" type="text" name="server" id="server" placeholder="support.juwolf.com">
                     </td>
                 </tr>
                 <tr>
                     <td class="py-2">Username</td>
                     <td class="py-2 text-right">
-                        <input class="p-1 border-solid border-2 rounded-lg border-black" type="text" name="username" id="username">
+                        <input class="p-1 border-solid border-2 rounded-lg border-white text-right bg-zinc-700" type="text" name="username" id="username">
                     </td>
                 </tr>
                 <tr>
                     <td class="py-2">Passwort</td>
                     <td class="py-2 text-right">
-                        <input class="p-1 border-solid border-2 rounded-lg border-black" type="password" name="password" id="password">
+                        <input class="p-1 border-solid border-2 rounded-lg border-white text-right bg-zinc-700" type="password" name="password" id="password">
                     </td>
                 </tr>
             </table>
-            <div class="w-full flex justify-between">
+            <div class="p-1 w-full flex justify-between">
                 <h2 class="text-lg text-red-500">Getrennt</h2>
                 <button class="border-solid bg-slate-700 hover:bg-slate-500 rounded-lg text-white p-2">Verbinden</button>
             </div>
@@ -33,9 +33,6 @@
                     <p class="p-1">Hier steht ein Log text</p>
                 </div>
             </div>
-        </div>
-        <div class="w-1/2 text-right">
-            <p>2</p>
         </div>
     </div>
 </div>
